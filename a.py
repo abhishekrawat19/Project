2d3775d77d49108c1da7bf0583cbb7b2f4b5e8a3
Abhishek Rawat <aabhishekrawattt@gmail.com>
2024-11-10 13:08:33 +0530

    bew

--- a.py
+++ a.py
@@ -1,12 +1,14 @@
-
 print("hleo")
-q=123
+q = 123
 print(q)
-#if you want to sk me how to  use the game i am herre fot it
-'''
+# if you want to sk me how to  use the game i am herre fot it
+"""
 cjisjcksdnm
 kcljnskc
 xsjncsnmc 
 cnsjnc
-'''
+"""
 print("abc")
+"""
+abchasjia
+"""
